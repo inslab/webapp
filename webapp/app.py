2ce2b859210a408186237258a952d8b6bbf6f4ac
Sunchan Lee <blurblah@blurblah.net>
2016-12-12 15:22:28 +0900

    Import numpy for test

--- webapp/app.py
+++ webapp/app.py
@@ -1,22 +1,30 @@
 import os
 
 from flask import Flask
+from flask import request
+import importlib
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     provider = str(os.environ.get('PROVIDER', 'world'))
     return 'Hello '+provider+'!'
 
+
 @app.route('/simplewebapp')
 def simple():
-    f = open('./big.txt', 'r')
-    lines = f.readlines()
-    f.close()
-
-    provider = str(os.environ.get('PROVIDER', 'world'))
-    return 'Hello '+provider+'!!'
+    #f = open('./big.txt', 'r')
+    #lines = f.readlines()
+    #f.close()
+    module = request.args.get('module')
+    if module == 'numpy':
+        importlib.import_module(module)
+        return 'Hello numpy!!'
+    else:
+        provider = str(os.environ.get('PROVIDER', 'world'))
+        return 'Hello '+provider+'!!!'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
